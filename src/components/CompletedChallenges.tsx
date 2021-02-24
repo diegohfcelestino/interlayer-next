6607a03c90be92fdcccebb44b9de053914410519
diegohfcelestino <diegohfcelestino@gmail.com>
2021-02-24 20:01:54 -0300

    Foi aplicado os contextos e iniciado a comunicação entre elementos

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/completedChallenges.module.css';
 
 export function CompletedChallenges(){
+    const { ChallengesCompleted } = useContext(ChallengesContext);
+
     return (
         <div className={styles.completedChallengesContainer}>
             <span>Desafios completos</span>
-            <span>5</span>
+            <span>{ChallengesCompleted}</span>
         </div>
     );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/global.css'
+import { useState } from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/global.css';
+
+import { ChallengesProvider } from '../contexts/ChallengeContext';
+
+
+function MyApp({ Component, pageProps }) {  
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
 }
 
 export default MyApp
