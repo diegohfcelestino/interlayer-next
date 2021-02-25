d59c9d139ed10afa954989f1dfe28b8693b209f5
diegohfcelestino <diegohfcelestino@gmail.com>
2021-02-25 12:27:10 -0300

    Continuamos evoluindo nossa aplicação, aprendemos ainda mais sobre a Context API, e aprendemos a como utilizar as notificações e sons do navegador para melhorar a experiência do usuário, tudo foi adicionado

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
+
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/diegohfcelestino.png" alt="Diego Henrique Ferreira" />
@@ -8,7 +13,7 @@ export function Profile() {
                 <strong>Diego Henrique Ferreira</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                     </p>
             </div>
         </div>
